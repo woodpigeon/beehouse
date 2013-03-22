5ac9ca84e7c8ee88d93a872311c18019a2c40303
Tim Crowe <tim@woodpigeon.com>
2013-03-22 09:59:17 +0000

    removed initialize_on_precompile to get heroku to work?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Beehouse::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-  config.assets.initialize_on_precompile = false
+  #config.assets.initialize_on_precompile = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
